a0f774cb1bfe0d148e1c89697e8089fea712ecf2
Brygetee Al-Shawkani <brygetee@gmail.com>
2025-08-31 19:02:38 -0400

    Corrected missed cases

--- main.py
+++ main.py
@@ -5,7 +5,45 @@ What you should know: Store key–value pairs for structured data.
 
 
 
+"""------------------Initial Code------------------"""
+#
+# supplies = {
+#     "pins": 100,
+#     "needles": 20,
+#     "cutting mat": 1,
+# }
+# supplies_list = list(supplies)
+# total = f"You have a total of {supplies['pins']} pins, {supplies['needles']} needles, and {supplies['cutting mat']} cutting mat."
+# def supplies_checker(project_type):
+#     return supplies.get(project_type, None)
+#
+# while True:
+#     status = input(f"Which item would you like to check the quantity of? {supplies_list}\n").lower()
+#     result = supplies_checker(status)
+#     if result:
+#         print(f"You have {result} {status}.")
+#         new_supplies_bought = input(f"Have you recently bought more supplies?\nIf so, which one? {supplies_list},If none type 'none'") #MAKE SURE IF THE USER WROTE IT WRONG THEN YOU KNOW HOW TO FIX IT, also if the user types no
+#         # if statement checking that the input is a key in the dictionary,
+#         if new_supplies_bought in supplies:
+#             new_supplies_amount_added = int(input(f"how many {new_supplies_bought} did you buy?\n"))
+#             #get dictionary value of existing amount to add to the new amount
+#             new_total_amount = supplies[new_supplies_bought] + new_supplies_amount_added
+#             #change the amount in the actual dictionary and print it.
+#             supplies[new_supplies_bought] = new_total_amount
+#             print(f"You now have {supplies[new_supplies_bought]} {new_supplies_bought}. ")
+#         elif new_supplies_bought == "none":
+#             #print statement printing all the supplies and their values
+#             print(total)
+#             break
+#         else:
+#             print("Choose a valid response")
+#         break
+#     else:
+#         print("Choose a valid response.")
 
+"""How to improve code
+1. To make the total statement print cutting mats when there is more than one, a function is needed.
+"""
 
 supplies = {
     "pins": 100,
@@ -13,6 +51,12 @@ supplies = {
     "cutting mat": 1,
 }
 supplies_list = list(supplies)
+
+def make_total():
+    return (f"You have a total of {supplies['pins']} pins, "
+            f"{supplies['needles']} needles, and "
+            f"{supplies['cutting mat']} cutting "
+            f"{'mats' if supplies['cutting mat'] > 1 else 'mat'}.")
 
 def supplies_checker(project_type):
     return supplies.get(project_type, None)
@@ -22,28 +66,22 @@ while True:
     result = supplies_checker(status)
     if result:
         print(f"You have {result} {status}.")
-        new_supplies_bought = input(f"Have you recently bought more supplies?\nIf so, which one? {supplies_list},If none type 'none'") #MAKE SURE IF THE USER WROTE IT WRONG THEN YOU KNOW HOW TO FIX IT, also if the user types no
+        while True:
+            new_supplies_bought = input(f"Have you recently bought more supplies?\nIf so, which one? {supplies_list}, If none type 'none'.\n")
+
         # if statement checking that the input is a key in the dictionary,
-        if new_supplies_bought in supplies:
-            new_supplies_amount_added = int(input(f"how many {new_supplies_bought} did you buy?\n"))
-            #get dictionary value of existing amount to add to the new amount
-            new_total_amount = supplies[new_supplies_bought] + new_supplies_amount_added
-            #change the amount in the actual dictionary and print it.
-            supplies[new_supplies_bought] = new_total_amount
-            print(f"You now have {supplies[new_supplies_bought]} {new_supplies_bought}")
-        elif new_supplies_bought == "none":
-            #TODO: Add a print statement printing all the supplies and their values
-            break
+            if new_supplies_bought in supplies:
+                new_supplies_amount_added = int(input(f"how many {new_supplies_bought} did you buy?\n"))
+                #get dictionary value of existing amount to add to the new amount
+                new_total_amount = supplies[new_supplies_bought] + new_supplies_amount_added
+                #change the amount in the actual dictionary and print it.
+                supplies[new_supplies_bought] = new_total_amount
+                print(make_total())
+                break
+            elif new_supplies_bought == "none":
+                #print statement printing all the supplies and their values
+                print(make_total())
+                break
+
         else:
-            print("Choose a valid response")
-
-        #
-        # print(new_supplies_amount)
-
-        # and add this to the value the user has input
-        #then return this value
-
-        break
-    else:
-        print("Choose a valid response.")
-
+            print("Choose a valid response.")
